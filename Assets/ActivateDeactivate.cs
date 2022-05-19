5a2f73c2102b14a5f1763ec054dee10fa6cca4dc
GaloResnik <101577483+GaloResnik@users.noreply.github.com>
2022-05-19 08:59:43 -0300

    Initial commit

---
+++ Assets/ActivateDeactivate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateDeactivate : MonoBehaviour
+{
+    public GameObject inputField;
+
+    public void ActivateandDeactivate()
+    {
+        //if (inputField.activeInHierarchy)
+        //{
+        //    inputField.SetActive(false);
+        //}
+        //else
+        //{
+        //    inputField.SetActive(true);
+        //}
+        inputField.SetActive(!inputField.activeInHierarchy);
+        
+    }
+}
